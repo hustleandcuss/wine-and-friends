1445bc4382930cc97ae767ac35d7fd913bcb335e
Olivia Lennerö <olivialennero@gmail.com>
2020-05-14 20:27:11 +0200

    xml

--- amplify/backend/function/randomwine2/src/main.py
+++ amplify/backend/function/randomwine2/src/main.py
@@ -4,8 +4,8 @@ import xml.etree.ElementTree as ET
 
 def random_wine_handler(event, _):
     # TODO: implement logic here
-    tree = ET.parse('https://www.systembolaget.se/api/assortment/products/xml')
+    tree = ET.parse('systembolaget.xml')
     root = tree.getroot()
-    root.tag
+    print root.tag
 
     return helpers.response("Red red wine!")
